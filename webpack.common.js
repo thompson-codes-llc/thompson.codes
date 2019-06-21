4b1ba41940b044ec1a5d05973da782b8cf532403
Bryan Thompson <bryan@thompson.codes>
2019-06-21 10:58:27 -0600

    Readded necessary /dist/

--- webpack.common.js
+++ webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
     resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
+        publicPath: "/dist/",
         filename: "bundle.js"
     }
 };
