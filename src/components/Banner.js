5141efbc0306059152ca25bd5919998dcdc45b8b
Bryan Thompson <bryan@thompson.codes>
2023-03-26 14:36:46 -0700

    cleanup logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "../styles/banner.css";
 
-import mountain from '../assets/mountain.png';
-
 export default class Banner extends Component {
   constructor() {
     super();
@@ -25,7 +23,7 @@ export default class Banner extends Component {
         <div className="banner">
           <h1 className="title_name first">THOMPSON</h1>
             <a href="/">
-              <img className="thompson_codes_logo" alt="Thompson Codes Logo" src={mountain} />
+              <img className="thompson_codes_logo" alt="Thompson Codes Logo" src="/logo.png" />
             </a>
           <h1 className="title_name second">CODES</h1>
         </div>
