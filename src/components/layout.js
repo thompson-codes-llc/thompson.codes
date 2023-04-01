f6a035e9f539eb2bc0ff17ddf8f9024057fbd4d9
Traci <tracir@gmail.com>
2023-04-01 14:54:51 -0700

    styling tweaks

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,57 @@
 import * as React from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Card } from '@mui/material';
 
 export const Layout = () => {
-    const [backgroundColor, setBackgroundColor] = React.useState("black");
-    const handleHover = () => {
-        const newColor = backgroundColor === "black" ? "white" : "black";
-        setBackgroundColor(newColor);
-    }
-
     return (
     <>
-        <Box backgroundColor="black" height="100%" margin="0" fixed />
+      <Card 
+        sx={{
+          height: "100%",
+        }}
+      >
+        <Box sx={{
+          bgcolor: "black",
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+        }} />
+        <Button 
+          variant="text" 
+          sx={{
+            color: "white",
+            fontSize: "80px", 
+            position: "fixed", 
+            bottom: "40%", 
+            right: "8%",
+          }}>
+          Codes
+        </Button>
+      </Card>
+
+      <Card sx={{height: "75%"}}>       
         <Box 
-            backgroundColor="black"
-            borderRadius="50%" 
-            border="4px solid white" 
-            width="2000px" 
-            height="2000px" 
-            position="absolute"  
-            bottom="10%" 
-            right="15%"
-            onMouseOver={() => handleHover()}
+          sx={{
+            bgcolor: 'white',
+            opacity: .8,
+            borderRadius: "50%",
+            border: "4px solid white",
+            width: "2000px", 
+            height: "2000px", 
+            position: "fixed",  
+            bottom: "5%",
+            right: "15%",
+          }}
         />
-        <Box 
-            backgroundColor="white"
-            borderRadius="50%" 
-            border="1.5px solid white"
-            component="span" 
-            width="1500px" 
-            height="1500px" 
-            position="absolute"  
-            bottom="20%" 
-            right="40%"
-            onMouseOver={() => handleHover()}
-        >
-        </Box>
-        <Box 
-            backgroundColor="black"
-            borderRadius="50%" 
-            border="1.5px solid white" 
-            width="1500px" 
-            height="1500px" 
-            position="absolute"  
-            bottom="40%" 
-            right="50%"
-            onMouseOver={() => handleHover()}
-        />
+        <Button variant="text" sx={{
+          color: "black", 
+          fontSize: "80px", 
+          position: "absolute", 
+          bottom: "50%", 
+          right: "35%",
+        }}>
+          Thompson
+        </Button>
+      </Card> 
     </>
     )
 }
