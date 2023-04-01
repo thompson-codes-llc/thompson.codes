4867e22ab013c253a7134134c5597fae4ce67eb1
Traci <tracir@gmail.com>
2023-03-31 19:12:29 -0700

    more boxes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Container } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 
 export const Layout = () => {
     const [backgroundColor, setBackgroundColor] = React.useState("black");
@@ -7,19 +7,43 @@ export const Layout = () => {
         const newColor = backgroundColor === "black" ? "white" : "black";
         setBackgroundColor(newColor);
     }
-    
+
     return (
     <>
         <Box backgroundColor="black" height="100%" margin="0" fixed />
         <Box 
             backgroundColor="black"
             borderRadius="50%" 
-            border="1.5px solid white" 
+            border="4px solid white" 
             width="2000px" 
             height="2000px" 
             position="absolute"  
             bottom="10%" 
-            right="20%"
+            right="15%"
+            onMouseOver={() => handleHover()}
+        />
+        <Box 
+            backgroundColor="white"
+            borderRadius="50%" 
+            border="1.5px solid white"
+            component="span" 
+            width="1500px" 
+            height="1500px" 
+            position="absolute"  
+            bottom="20%" 
+            right="40%"
+            onMouseOver={() => handleHover()}
+        >
+        </Box>
+        <Box 
+            backgroundColor="black"
+            borderRadius="50%" 
+            border="1.5px solid white" 
+            width="1500px" 
+            height="1500px" 
+            position="absolute"  
+            bottom="40%" 
+            right="50%"
             onMouseOver={() => handleHover()}
         />
     </>
