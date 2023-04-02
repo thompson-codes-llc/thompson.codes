1f2d177ceab97aba9c10dd2ed064cc19f4e2ff2b
Traci <tracir@gmail.com>
2023-04-02 10:20:24 -0700

    basic layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,68 +1,5 @@
-// import * as React from 'react';
-// import { Box, Button, Card } from '@mui/material';
-// import { Menu } from './menu';
-
-// export const Layout = () => {
-//     return (
-//     <>
-//       <Card 
-//         sx={{
-//           height: "100%",
-//         }}
-//       >
-//         <Box sx={{
-//           bgcolor: "black",
-//           height: "100%",
-//           width: "100%",
-//           position: "absolute",
-//         }} />
-//         <Button 
-//           variant="text" 
-//           sx={{
-//             color: "white",
-//             fontSize: "80px", 
-//             position: "fixed", 
-//             bottom: "40%", 
-//             right: "10%",
-//           }}>
-//           Codes
-//         </Button>
-//       </Card>
-
-//       <Card sx={{height: "35%"}}>       
-//         <Box 
-//           sx={{
-//             bgcolor: 'white',
-//             borderRadius: "50%",
-//             border: "4px solid white",
-//             width: "2000px", 
-//             height: "2000px", 
-//             position: "fixed",  
-//             bottom: "15%",
-//             right: "15%",
-//           }}
-//         />
-//         <Menu sx={{
-//           border: "10px solid pink"
-//         }}/>
-//         <Button variant="text" sx={{
-//           color: "black", 
-//           fontSize: "80px", 
-//           position: "absolute", 
-//           bottom: "50%", 
-//           right: "35%",
-//           marginTop: "20px"
-//         }}>
-//           Thompson
-//         </Button>
-//       </Card> 
-//     </>
-//     )
-// }
-
 import * as React from 'react';
-import { Box, Button, Card } from '@mui/material';
-import { Menu } from './menu';
+import { Box, Card, Typography } from '@mui/material';
 
 export const Layout = () => {
   return (
@@ -71,26 +8,46 @@ export const Layout = () => {
         display: "flex",
         flexDirection: "row",
         bgcolor: "black",
-        height: "100%"
+        height: "100%",
       }}
     >
       <Box
         sx={{
           bgcolor: "white",
-          borderRadius: "0 55% 90% 20%",
-          flex: 2,
-          height: "150%",
-          margin: "-30% 0 5% -10%"
+          borderRadius: "0 20% 60% 0%",
+          flex: 1.5,
+          height: "125%",
+          margin: "-17% 0 0% 0%"
         }}
       >
+        <Typography 
+          variant='h1' 
+          sx={{
+            color: "black",
+            marginTop: "60%",
+            marginLeft: "40%"
+          }}
+        >
+          Thompson
+        </Typography>
       </Box>
       <Box
         sx={{
           bgcolor: "black",
           flex: 1,
           height: "1000px",
+
         }}
       >
+        <Typography 
+          variant='h1' 
+          sx={{
+            color: "white",
+            marginTop: "65%",
+          }}
+        >
+          Codes
+        </Typography>
       </Box>
     </Card>
   );
