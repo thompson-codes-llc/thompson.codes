6deed81225cb312c6fa6cd93ab2d7e7fefba69fe
Traci <tracir@gmail.com>
2023-04-02 12:28:18 -0700

    no-edit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, ImageList, ImageListItem, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import { Layout } from '../components/layout';
 import MailIcon from '@mui/icons-material/MailOutline';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
