655e45dbdc620fec2be463d63c51455972e4a17b
Traci <tracir@gmail.com>
2023-04-02 11:38:06 -0700

    icons and contact links

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,34 +1,62 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Box, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import { Layout } from '../components/layout';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Contact = () => {
   return (
     <Layout left={
+      <>
         <Typography
           variant="h3"
           sx={{
             color: "black",
             marginTop: "50%",
-            marginLeft: "5%",
-            marginRight: "5%"
+            marginLeft: "20%",
           }}
         >
-          How to reach us:
+          Find Us
         </Typography>
+        <List>
+          <ListItem component="a" target="_blank" href="mailto:contact@thompson.codes">
+            <ListItemIcon>
+              <MailOutlineIcon />
+            </ListItemIcon>
+            <ListItemText>contact@thompson.codes</ListItemText>
+          </ListItem>
+          <ListItem component="a" target="_blank" href="https://www.linkedin.com/in/snowboardtechie/">
+            <ListItemIcon>
+              <LinkedInIcon />
+            </ListItemIcon>
+            <ListItemText>LinkedIn: Bryan Thompson</ListItemText>
+          </ListItem>
+          <ListItem component="a" target="_blank" href="https://www.linkedin.com/in/tracihthompson/">
+            <ListItemIcon>
+              <LinkedInIcon />
+            </ListItemIcon>
+            <ListItemText>LinkedIn: Traci Thompson</ListItemText>
+          </ListItem>
+        </List>
+      </>
     } right={
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center"
+        }}
+      >
         <Typography
           variant="h3"
           sx={{
             color: "white",
-            marginTop: "70%",
-            marginLeft: "0%"
+            marginTop: "30%",
           }}
         >
-          We can be reached via email at contact@thompson.codes 
-        </Typography>}
-      />
-
+          Have a Question? 
+        </Typography>
+      </Box>
+    }/>
   );
 }
 
