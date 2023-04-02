095575bc81a9127fed2aff32cafbeda5b19d75ff
Traci <tracir@gmail.com>
2023-04-02 14:03:16 -0700

    theme and eslint configuration start

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import './App.css';
 import About from './pages/about';
 import Contact from './pages/contact';
 import { Home } from './components/home';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+
+});
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 
