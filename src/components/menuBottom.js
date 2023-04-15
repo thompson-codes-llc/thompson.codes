381e78ecd479c5f78e968d24068e25319cebda65
Traci <tracir@gmail.com>
2023-04-15 11:14:36 -0700

    adjust menu bottom

--- src/components/menuBottom.js
+++ src/components/menuBottom.js
@@ -11,7 +11,7 @@ export const MenuBottom = () => {
           lg: "-20%",
         },
         marginLeft: {
-          xs: "20%",
+          xs: "15%",
           lg: "30%",
         },
         marginBottom: {
