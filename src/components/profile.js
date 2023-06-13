c1338a394dff2210151a5d8773173d86ad7dfb61
Traci <tracir@gmail.com>
2023-06-12 18:44:13 -0700

    responsive styling

--- src/components/profile.js
+++ src/components/profile.js
@@ -6,28 +6,51 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import CoPresentIcon from "@mui/icons-material/CoPresent";
 
 export const Profile = ({ image, name, linkedInUrl }) => {
+  const boxStyles = {
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const detailStyles = {
+    marginY: "1%",
+    padding: 0,
+  };
+
+  const iconStyles = {
+    height: "100px",
+    borderRadius: "25%",
+    boxShadow: 2,
+  };
+
+  const listStyles = {
+    display: "flex",
+    gap: "5%",
+    marginY: "5%",
+    justifyContent: "center",
+  };
+
+  const textStyles = {
+    color: "rgb(165,170,154)",
+    fontSize: {
+      xs: "1.5rem",
+      sm: "2rem",
+      md: "2.5rem",
+    },
+  };
+
   return (
-    <ListItem sx={{ display: "flex", gap: "5%", marginY: "5%" }}>
+    <ListItem sx={{ ...listStyles }}>
       <ListItemIcon>
-        <Box
-          component="img"
-          sx={{
-            height: "100px",
-            borderRadius: "25%",
-            boxShadow: 2,
-          }}
-          alt={name}
-          src={image}
-        />
+        <Box component="img" sx={{ ...iconStyles }} alt={name} src={image} />
       </ListItemIcon>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <Typography sx={{ color: "rgb(165,170,154)" }} variant="h4">
+      <Box sx={{ ...boxStyles }}>
+        <Typography sx={{ ...textStyles, color: "rgb(165,170,154)" }}>
           {name} Thompson
         </Typography>
-        <Typography sx={{ color: "rgb(165,170,154)" }} variant="h4">
+        <Typography sx={{ ...textStyles, color: "rgb(165,170,154)" }}>
           Co-founder
         </Typography>
-        <Box sx={{ marginY: "1%", padding: "0" }}>
+        <Box sx={{ ...detailStyles }}>
           <Link target="_blank" href={linkedInUrl}>
             <ListItemIcon>
               <LinkedInIcon fontSize="large" />
