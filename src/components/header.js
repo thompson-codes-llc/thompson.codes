3e2ca13be51b143968e43a389d7d5b090f113f21
Traci <tracir@gmail.com>
2023-09-16 17:37:35 -0700

    style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,65 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Box, Button, Card, Typography } from "@mui/material";
-import image from "../assets/istock-header.png";
+import image from "../assets/beach_background.jpg";
 import React from "react";
 
 export const Header = () => {
   return (
-    <Box display={"flex"} flexDirection={"row"}>
-      <Box display="flex" flexDirection={"column"}>
-        <Typography fontFamily={"Josefin Sans"} fontSize="80px">
+    <Box display={"flex"} flexDirection={"row"} sx={{}}>
+      <Box
+        display="flex"
+        flexDirection={"column"}
+        position="absolute"
+        sx={{
+          gap: "20px",
+          paddingLeft: "5%",
+          top: "525px",
+          zIndex: 1,
+        }}
+      >
+        <Typography
+          fontFamily={"Josefin Sans"}
+          fontSize="300px"
+          sx={{
+            color: "#295073",
+            fontStyle: "normal",
+            fontWeight: 400,
+            lineHeight: "normal",
+            letterSpacing: "-2.4px",
+          }}
+        >
           Your business, made simple
         </Typography>
-        <Typography fontFamily={"Lato"} fontSize="40px">
+        <Typography
+          fontFamily={"Lato"}
+          fontSize="125px"
+          sx={{
+            color: "#1763A6",
+          }}
+        >
           Free up your time so that you can focus on what matters most.
         </Typography>
+        {/* TODO: Set CTA hover state */}
         <Button
           sx={{
-            borderRadius: "20px",
+            borderRadius: "30px",
             backgroundColor: "var(--Orange-2, #BB6E36)",
-            width: "426px",
+            marginTop: "75px",
+            width: "25%",
+            height: "150px",
           }}
         >
           <Typography
             textAlign={"center"}
             sx={{
               color: "#FFF",
-              fontSize: "32px",
+              fontFamily: "Lato",
+              fontSize: "45px",
               fontStyle: "normal",
-              fontWeight: 400,
+              fontWeight: 600,
               lineHeight: "normal",
-              letterSpacing: "1.28px",
+              letterSpacing: "3px",
             }}
           >
             Schedule a consultation
@@ -38,9 +68,10 @@ export const Header = () => {
       </Box>
       <Box
         component="img"
+        position="absolute"
         sx={{
-          ml: "auto",
-          mr: "-275px", // trim image instead
+          right: 0,
+          width: "80%",
         }}
         alt="working-remotely"
         src={image}
