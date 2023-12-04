046914b24076a06cd0fb5a634ebb0445652e4757
Bryan Thompson <bryan@thompson.codes>
2023-12-03 17:24:46 -0800

    extract testimonials css

--- src/components/home/testimonials.js
+++ src/components/home/testimonials.js
@@ -1,49 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Box, Card, MobileStepper, Stack, Typography } from "@mui/material";
+import "../../styles/components/home/testimonials.css";
 
 const Testimonials = () => {
   return (
-    <Box
-      sx={{
-        background:
-          "linear-gradient(180deg, #FCFCFC 0.0%, rgba(252, 251, 251, 0.25) 0.1%, rgba(202, 142, 99, 0.45) 20.84%, RGBA(187, 110, 54, .6) 50%, rgba(197, 132, 84, 0.45) 80.94%, rgba(202, 142, 99, 0.25) 88.54%, #FCFCFC 100%)",
-        minHeight: "700px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: "-10px",
-      }}
-    >
-      <Card
-        sx={{
-          height: "400px",
-          width: "65%",
-          borderRadius: "16px",
-          boxShadow: "6px 6px 20px 0px rgba(41, 80, 115, 0.46)",
-          backgroundColor: "#FCFCFC",
-        }}
-      >
-        <Stack
-          sx={{
-            alignItems: "center",
-            height: "100%",
-            justifyContent: "space-evenly",
-          }}
-        >
-          <Typography
-            sx={{
-              color: "#1763A6",
-              textAlign: "center",
-              fontFamily: "Josefin Sans",
-              fontSize: "40px",
-              fontStyle: "normal",
-              fontWeight: 400,
-              lineHeight: "normal",
-              letterSpacing: "-2.4px",
-              padding: "50px",
-            }}
-          >
+    <Box className="testimonials">
+      <Card className="testimonials-card">
+        <Stack className="testimonials-card-content">
+          <Typography className="testimonial">
             “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.”
           </Typography>
@@ -52,7 +17,6 @@ const Testimonials = () => {
             steps={3}
             activeStep={0}
             position="static"
-            sx={{}}
             backButton={undefined}
             nextButton={undefined}
           />
