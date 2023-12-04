e06705c39d181b1c897bbbf8ea4f4913c7f0ce42
Bryan Thompson <bryan@thompson.codes>
2023-12-03 17:20:54 -0800

    extract contact us css

--- src/components/home/contact.js
+++ src/components/home/contact.js
@@ -1,6 +1,6 @@
 import { Box, Button, FormControl, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-import backgroundImage from "../../assets/woman-dock.jpg";
+import "../../styles/components/home/contact.css";
 
 export const Contact = () => {
   const [message, setMessage] = useState("");
@@ -14,67 +14,11 @@ export const Contact = () => {
   };
 
   return (
-    <Box
-      sx={{
-        height: "100%",
-        backgroundSize: "cover",
-        backgroundImage: `linear-gradient(to bottom, white, transparent 75%), url(${backgroundImage})`,
-        backgroundRepeat: "no-repeat",
-        minHeight: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-end",
-      }}
-    >
-      <Box
-        id="contact-us"
-        sx={{
-          height: "475px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "450px",
-          borderRadius: "16px",
-          boxShadow: "6px 6px 20px 0px rgba(41, 80, 115, 0.46)",
-          backgroundColor: "#FCFCFC",
-          marginRight: "70%",
-        }}
-      >
-        <Typography
-          fontFamily={"Josefin Sans"}
-          fontSize="80px"
-          sx={{
-            color: "#295073",
-            fontStyle: "normal",
-            fontWeight: 400,
-            lineHeight: "normal",
-            letterSpacing: "-2.4px",
-            textAlign: "center",
-          }}
-        >
-          Contact Us
-        </Typography>
-        <FormControl
-          onSubmit={handleSubmit}
-          sx={{
-            paddingTop: "25px",
-            minWidth: "80%",
-          }}
-        >
-          <Typography
-            sx={{
-              color: "var(--Medium-Blue, #1763A6)",
-              textAlign: "center",
-              fontFamily: "Josefin Sans",
-              fontSize: "28px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "normal",
-              letterSpacing: "-1.44px",
-              paddingBottom: "15px",
-            }}
-          >
+    <Box className="contact">
+      <Box id="contact-us">
+        <Typography className="contact-title">Contact Us</Typography>
+        <FormControl onSubmit={handleSubmit} className="contact-form">
+          <Typography className="contact-form-title">
             What would you like to discuss?
           </Typography>
           <TextField
@@ -83,22 +27,13 @@ export const Contact = () => {
             rows={4}
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            sx={{
-              paddingBottom: "25px",
-            }}
+            className="contact-form-input"
           />
           <Button
             id="email-button"
             type="submit"
             variant="contained"
-            color="primary"
             onClick={handleSubmit}
-            sx={{
-              width: "40%",
-              display: "flex",
-              justifyContent: "center",
-              margin: "auto",
-            }}
           >
             Email us
           </Button>
